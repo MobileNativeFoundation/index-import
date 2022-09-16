e11e2d47a2c3a01dd20c8a3b7243d108e493dbce
Keith Smiley <keithbsmiley@gmail.com>
2022-09-16 09:49:48 -0700

    fixes
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- .github/build-index-store.sh
+++ .github/build-index-store.sh
@@ -3,11 +3,13 @@
 set -euo pipefail
 set -x
 
+rm -rf /tmp/indexstorebuild
 mkdir -p /tmp/indexstorebuild
+cd /tmp/indexstorebuild
 git clone --branch swift/release/5.7 --depth 1 https://github.com/apple/llvm-project.git
 
 cd llvm-project
 mkdir -p build
 
-cmake -B build llvm -DLLVM_ENABLE_PROJECTS=clang
+cmake -B build llvm -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --target IndexStore FileCheck
